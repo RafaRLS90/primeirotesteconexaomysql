fdaca557e7037d618595943dfeeff517542fd2ef
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2023-04-07 13:04:58 -0300

    tranalhando com Pool de conexões

--- src/loja_virtual_repository/ConnectionFactory.java
+++ src/loja_virtual_repository/ConnectionFactory.java
@@ -1,14 +1,24 @@
 package loja_virtual_repository;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+import javax.sql.DataSource;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class ConnectionFactory {
 	
+	public DataSource dataSource;
+	
+	public ConnectionFactory() {
+		ComboPooledDataSource comboPoolDataSource = new ComboPooledDataSource();
+		comboPoolDataSource.setJdbcUrl("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC");
+		comboPoolDataSource.setUser("root");
+		comboPoolDataSource.setPassword("rafadev1990java");
+		
+		this.dataSource = comboPoolDataSource;
+	}
+	
 	public Connection recuperarConexao() throws SQLException {
-		return DriverManager
-				.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "rafadev1990java");
-			
+		return this.dataSource.getConnection();
 	}
 }
