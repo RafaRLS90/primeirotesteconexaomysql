8f76821b2f6d6abaa92591c2bba2c356273d934b
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2023-04-08 23:02:09 -0300

    utilizando INNER JOIN

--- src/modelo/Categoria.java
+++ src/modelo/Categoria.java
@@ -1,8 +1,12 @@
 package modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Categoria {
 	private Integer id;
 	private String nome;
+	private List<Produto> produtos = new ArrayList<Produto>();
 	
 	public Categoria(Integer id, String nome) {
 		this.id = id;
@@ -12,7 +16,18 @@ public class Categoria {
 	public String getNome() {
 		return nome;
 	}
+
+	public int getId() {	
+		return id;
+	}
+
+	public void adicionar(Produto produto) {
+		produtos.add(produto);
+		
+	}
 	
-	
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
 	
 }
